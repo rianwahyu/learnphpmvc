3c33074827d1d40835e41e3cee0e9a9641995257
Rian Wahyu <rianwahyu26@gmail.com>
2022-03-09 14:57:52 +0700

    database Wrappe

--- app/model/Mahasiswa_model.php
+++ app/model/Mahasiswa_model.php
@@ -3,6 +3,16 @@
 class Mahasiswa_model
 {
 
+    private $table = 'mahasiswa';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+
+
     /* private $mhs = [
         [
             "nama" => "Rian",
@@ -27,24 +37,23 @@ class Mahasiswa_model
 
     ]; */
 
-    private $dbh;
-    private $stmt;
 
-    public function __construct()
+
+    public function getAllMahasiswa()
     {
-        $dsn = 'mysql:host=localhost;dbname=phpmvc;';
+        // $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        // $this->stmt->execute();
+        // return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        //return $this->mhs;
 
-        try{
-            $this->dbh = new PDO($dsn, 'root', '');            
-        }catch(PDOException $e){
-            die($e->getMessage());
-        }
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
     }
 
-    public function getAllMahasiswa(){
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-        //return $this->mhs;
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
