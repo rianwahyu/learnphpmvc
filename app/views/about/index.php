5ccb6fdda36969452ce26accb86dc25b45ba63bd
Rian Wahyu <rianwahyu26@gmail.com>
2022-03-09 11:26:24 +0700

    implementasi Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-<h1>About Page</h1>
+<div class="container">
+    <h1 class="mt-4">About Page</h1>
+    <img src="<?= BASE_URL?>/img/myprofile.jpg"  alt="Rian Wahyu" width="200" class="rounded-circle shadow"/>
+    <p>Halo nama saya <?= $data['nama'] ?>, pekeraan saya <?= $data['pekerjaan'] ?>, umur saya <?= $data['umur'] ?></p>
 
-<p>Halo nama saya <?= $data['nama'] ?>, pekeraan saya <?= $data['pekerjaan'] ?>, umur saya <?= $data['umur'] ?></p>
+</div>
